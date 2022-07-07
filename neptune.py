ff355990b84a35b4009a22a330296de3ba951db4
Kamran Pulatov <84636918+WrldEngine@users.noreply.github.com>
2022-07-07 20:57:16 +0500

    Update neptune.py

--- neptune.py
+++ neptune.py
@@ -8,8 +8,22 @@ import os
 import subprocess
 import sys
 import threading
+from colorama import init, Fore, Back, Style
 
-bnnr = """
+init()
+RESET = Fore.RESET
+MAGENTA = Fore.MAGENTA
+YELLOW = Fore.YELLOW
+RED = Fore.RED
+BLACK = Fore.BLACK
+
+bg_reset = Back.RESET
+bg_red = Back.RED
+bg_yellow = Back.YELLOW
+bg_white = Back.WHITE
+bg_mag = Back.MAGENTA
+
+bnnr = f"""{MAGENTA}
  _   _            _                    
 | \ | |          | |                   
 |  \| | ___ _ __ | |_ _   _ _ __   ___ 
@@ -17,8 +31,8 @@ bnnr = """
 | |\  |  __/ |_) | |_| |_| | | | |  __/
 |_| \_|\___| .__/ \__|\__,_|_| |_|\___|
            | |                         
-           |_| By Pulatov Kamran
-Pulatov Kamran Development, 2022 | inst: @callistodev1
+           |_| {RESET}
+{bg_yellow}{BLACK}Pulatov Kamran Development (c), 2022 | inst: @callistodev1 | Author: Pulatov Kamran{bg_reset}{RESET}
 """
 
 n_thread = 200
@@ -30,25 +44,32 @@ parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers(help="commands")
 
 net_scan = subparsers.add_parser('net', help='End point of range IP addresses')
-net_scan.add_argument("-t", dest="tar", help="IP address")
+net_scan.add_argument("-i", dest="tar", help="IP address")
 net_scan.add_argument("-e", default="255", dest="end", help="End point of range IP addresses")
 
 port_scan = subparsers.add_parser('port', help='Scanning open ports of domain')
 port_scan.add_argument("-i", dest="ip", help="Scanning open ports of domain")
-port_scan.add_argument("-r", default="65535", dest="range", help="End point of range ports")
+port_scan.add_argument("-p", default="65535", dest="range", help="End point of range ports")
+
+sender = subparsers.add_parser('comm', help='Connecting and sending requests to device')
+sender.add_argument("-i", dest="host", help="IP address of domain/device")
+sender.add_argument("-c", dest="port", help="Domain's/devise's port")
+
 args = parser.parse_args()
+
 try:
     vars(args)["action"] = sys.argv[1]
-except:
-    print("Error ! Invalid command")
-
+except IndexError as e:
+    parser.print_help()
+    sys.exit()
+    
 def scan_Ip(ip):
     addr = net + str(ip)
     param = '-n' if name=='nt' else '-c'
     command = ['ping', param, '1', addr]
     result = subprocess.run(command, stdin=subprocess.DEVNULL, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, creationflags=0x08000000)
     if result.returncode == 0 and b'TTL=' in result.stdout:
-        print(f"[+] - {addr:13} - ON")
+        print(f"{YELLOW}[+] - {addr:13} - ON{RESET}")
 
 def mainf(host, ports):
     global q
@@ -78,7 +99,7 @@ def port_scan(port):
             pass
     else:
         with print_lock:
-            print(f"[+] {host:15} | {port:5} | OPEN")
+            print(f"{YELLOW}[+] {host:15} | {port:5} | OPEN {RESET}")
     finally:
         s.close()
 
@@ -90,16 +111,17 @@ if args.action == 'port':
     ports = [p for p in range(1, int(port))]
     
     print(bnnr)
-    print(f"IP: {ip_org}, HOST: {name}")
-    print(f"RANGE: 1 --> {port}")
+    print(f"{RED}IP: {YELLOW}{ip_org}{RESET}{RED}, HOST:{YELLOW}{name}{RESET}")
+    print(f"{RED}RANGE: 1 --> {port}{RESET}")
 
     start = time.time()
-    print("---------------")
+    z = len(mainf(ip_org, ports))
+    print(f"{YELLOW}-{RESET}" * z)
     mainf(ip_org, ports)
-    print("---------------")
+    print(f"{YELLOW}---------------{RESET}")
 
     total = time.time() - start
-    print(f"Scanning time: %s sec" % (round(total),))
+    print(f"{RED}Scanning time: %s sec{RESET}" % (round(total),))
 
 elif args.action == 'net':
     print(bnnr)
@@ -110,7 +132,7 @@ elif args.action == 'net':
     start_point = net_split[3]
     end_point = args.end
 
-    print(f"Started range {net+f'{start_point}'} --> {net+f'{end_point}'}")
+    print(f"{RED}Started range {YELLOW}{net+f'{start_point}'}{RESET}{RED} --> {YELLOW}{net+f'{end_point}'}{RESET}\n")
     start = time.time()
     for ip in range(int(start_point), int(end_point)):
         if ip == int(net_split[3]):
@@ -120,4 +142,20 @@ elif args.action == 'net':
 
     potoc.join()
     total = time.time() - start
-    print(f"Scanning time: %s sec" % (round(total),))
+    print(f"\n{RED}Scanning time: %s sec{RESET}" % (round(total),))
+
+elif args.action == 'comm':
+    print(bnnr)
+    host = args.host 
+    port = args.port
+
+    print(f"{YELLOW}Connection to {host}:{port} ---{RESET}")
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, int(port)))
+    data = s.recv(4096)
+    print(data)
+    while len(data) != 0:
+        n = input()
+        s.sendall(n.encode())
+    s.close()
